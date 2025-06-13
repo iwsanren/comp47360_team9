e832ea887a2c53bf1562464eccd6ba124f5f97bc
peja016 <jerry79525@gmail.com>
2025-06-13 20:12:57 +0100

    updated weather api data

--- frontend/src/app/api/weather/route.js
+++ frontend/src/app/api/weather/route.js
@@ -1,20 +1,30 @@
-export async function POST(res) {
+export async function POST() {
   const API_KEY = process.env.OPENWEATHER_API_KEY; // use .env to get api key.
 
   // lat and lon of Manhattan
-  const lat = 40.4342;
-  const lon = -73.5939;
+  const lat = 40.728333;
+  const lon = -73.994167;
 
   try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=imperial&appid=${API_KEY}`
+    const currentData = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`
     );
 
-    if (!response.ok) {
+    const hourlyData = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`
+    );
+
+    if (hourlyData.status !== 200 || currentData.status !== 200) {
       throw new Error('Failed to fetch weather data');
     }
 
-    const weatherData = await response.json();
+    const currentWeatherData = await currentData.json();
+    const hourlyWeatherData = await hourlyData.json();
+
+    const weatherData = {
+      current: currentWeatherData,
+      hourly: hourlyWeatherData
+    }
 
     return Response.json(weatherData, { status: 200 });
 
