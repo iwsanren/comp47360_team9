618edd1e77088aff6ae373806989b4022a7bb0b7
peja016 <jerry79525@gmail.com>
2025-07-14 00:53:27 +0100

    updated auth function & set Interval to refresh webpage

--- webapp/src/app/containers/AuthWrapper.tsx
+++ webapp/src/app/containers/AuthWrapper.tsx
@@ -6,10 +6,13 @@ export default function AuthWrapper({ children }: Readonly<{
     children: React.ReactNode;
   }>) {
   useEffect(() => {
+    const interval = setInterval(() =>  {
       fetch('/api/validation', {
-      method: 'POST',
-      credentials: 'include',
-    });
+        method: 'POST',
+        credentials: 'include',
+      });
+    }, 59 * 50 * 1000) // refresh every 59 mins
+    return () => clearInterval(interval);
   }, []);
 
   return <>{children}</>;
