1226f7f6df510b4b464d5cdb92e24e4490f7b820
peja016 <jerry79525@gmail.com>
2025-07-14 00:37:56 +0100

    changed vaildation secure value

--- webapp/src/app/api/validation/route.js
+++ webapp/src/app/api/validation/route.js
@@ -7,7 +7,7 @@ export async function POST() {
   const res = NextResponse.json({ ok: true });
   res.cookies.set('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    // secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
     maxAge: 3600,
     path: '/',
