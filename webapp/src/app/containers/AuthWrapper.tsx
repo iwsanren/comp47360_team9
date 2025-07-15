ba39290dbb5d44b13cfb42367a72e59e586b920b
peja016 <jerry79525@gmail.com>
2025-07-15 01:24:22 +0100

    updated AuthWrapper

--- webapp/src/app/containers/AuthWrapper.tsx
+++ webapp/src/app/containers/AuthWrapper.tsx
@@ -2,23 +2,20 @@
 
 import { useEffect } from 'react';
 
+const validation = () => fetch('/api/validation', {
+  method: 'POST',
+  credentials: 'include',
+});
+
 export default function AuthWrapper({ children }: Readonly<{
     children: React.ReactNode;
   }>) {
   useEffect(() => {
     // Trigger the validation request immediately on component mount
-      fetch('/api/validation', {
-      method: 'POST',
-      credentials: 'include',
-    });
+    validation()
 
     // Then repeat the validation request every 59 minutes
-    const interval = setInterval(() =>  {
-      fetch('/api/validation', {
-        method: 'POST',
-        credentials: 'include',
-      });
-    }, 59 * 60 * 1000); // 59 minutes
+    const interval = setInterval(() => validation(), 59 * 60 * 1000); // 59 minutes
 
     // Clear the interval when the component unmounts
     return () => clearInterval(interval);
