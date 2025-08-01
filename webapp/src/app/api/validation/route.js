45e3f94aecb05775c09b864ddb9f6d89fb1ca8d9
peja016 <jerry79525@gmail.com>
2025-08-01 18:13:28 +0100

    Add a new feature to refresh the selectable time options when the hour changes.

--- webapp/src/app/api/validation/route.js
+++ webapp/src/app/api/validation/route.js
@@ -1,14 +1,17 @@
 import { NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
 
-import { withAuthAndTracking } from "../../../middleware/requestTracker";
+import { withRequestTracking } from '../../../middleware/requestTracker';
 
-async function handler(req, payload) {
-  // Generate new token with extended expiration
-  const newToken = jwt.sign({ source: 'Manhattan_My_Way' }, process.env.JWT_SECRET, { expiresIn: '1h' });
+async function handler() {
+  const token = jwt.sign(
+    { source: 'Manhattan_My_Way' },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  );
 
   const res = NextResponse.json({ ok: true });
-  res.cookies.set('token', newToken, {
+  res.cookies.set('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV == 'production',
     sameSite: 'strict',
@@ -16,8 +19,8 @@ async function handler(req, payload) {
     path: '/',
   });
 
-  console.log(`Token validation and refresh successful for source: ${payload.source}`);
+  console.log('New JWT token generated for Manhattan_My_Way');
   return res;
 }
 
-export const POST = withAuthAndTracking(handler, 'API_VALIDATION');
+export const POST = withRequestTracking(handler, 'API_TOKEN');
--- webapp/src/app/containers/AuthWrapper.tsx
+++ webapp/src/app/containers/AuthWrapper.tsx
@@ -4,47 +4,48 @@ import { useEffect, useState } from 'react';
 
 const validateToken = async (setReady: any) => {
   try {
-    const res = await fetch('/api/validation', {
+    await fetch('/api/validation', {
       method: 'POST',
       credentials: 'include',
-    })
-    if (!res.ok) {
-      console.warn("Token invalid or missing, requesting new token...");
-      await fetch('/api/token', {
-        method: 'POST',
-        credentials: 'include',
-      })
-    }
+    });
     setReady(true);
   } catch (err) {
-    console.error("Failed to validate token:", err);
-    setReady('Please refresh the website, thanks')
+    console.error('Failed to validate token:', err);
+    setReady('Please refresh the website, thanks');
   }
 };
 
-const AuthWrapper = ({ children }: Readonly<{
-    children: React.ReactNode;
-  }>) => {
+const AuthWrapper = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
     // Trigger the validation request immediately on component mount
-    validateToken(setReady)
+    validateToken(setReady);
 
     // Then repeat the validation request every 50 minutes
-    const interval = setInterval(() => validateToken(setReady), 50 * 60 * 1000); // 50 minutes
+    const interval = setInterval(() => {
+      validateToken(setReady);
+    }, 50 * 60 * 1000); // 50 minutes
 
     // Clear the interval when the component unmounts
     return () => {
       clearInterval(interval);
-    }
-}, []);
+    };
+  }, []);
   if (typeof ready == 'string') {
-    return <div className="flex justify-center items-center h-[100dvh] text-xl">{ready}</div>;
+    return (
+      <div className="flex justify-center items-center h-[100dvh] text-xl">
+        {ready}
+      </div>
+    );
   }
   if (ready) {
     return <>{children}</>;
   }
-}
+};
 
-export default AuthWrapper
+export default AuthWrapper;
